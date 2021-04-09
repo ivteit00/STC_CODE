81fb37d2a079e88a0d423fd7aad00b19b61640ba
Ivan Filip Terzic <ivteit00@hs-esslingen.de>
2021-04-09 11:11:02 +0200

    working on dummy data

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ db = SQLAlchemy()
 DB_NAME = 'database.db'
 
 
-def create_app(config_name):
+def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '9QxEIB84nNKgxjz9ahPjM2HRtwrERAli4PGtjRjV'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
@@ -43,3 +43,18 @@ def create_database(app):
     if not path.exists('app/'+DB_NAME):
         db.create_all(app=app)
         print('Created database!')
+
+
+def create_dummy_users():
+    from .models import User, Role
+
+    worker_role = Role(name='worker')
+    chef_role = Role(name='chef')
+    hr_role = Role(name='hr')
+
+    test_user_1 = User(email='test1@gmail.de',
+                       password='password', full_name='Max Mustermann')
+    test_user_1 = User(email='test2@gmail.de',
+                       password='password', full_name='John Doe')
+    test_user_1 = User(email='test3@gmail.de',
+                       password='password', full_name='Anita John')
